a4da64da60ce134e13ddcaaeea4a3befa3cc5986
Ekaterina Kirilenko <katerina_sanway@mail.ru>
2020-02-25 15:02:07 +0300

    feat: completed task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 module.exports = function reverse (n) {
-  
+    
+let str = String(n);
+let newStr = "";
+
+    if (n >= 0) {
+        for (let i = str.length - 1; i >= 0; i--) {
+            newStr += str[i];
+        }
+    } else {
+        for (let i = str.length - 1; i > 0; i--) {
+            newStr += str[i];
+        }        
+    }
+
+    return newStr;
+
 }
